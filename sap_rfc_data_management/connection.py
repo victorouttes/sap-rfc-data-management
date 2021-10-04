c27ad10279a48fe81359fbb54ef988e5126a9a88
victor <victor.outtes@gmail.com>
2021-10-04 17:24:47 -0300

    closes #1

--- sap_rfc_data_management/connection.py
+++ sap_rfc_data_management/connection.py
@@ -3,31 +3,25 @@ from pyrfc import Connection
 
 class SAPConnection:
     def __init__(self,
-                 host: str,
-                 service: str,
-                 group: str,
-                 sysname: str,
                  client: str,
                  lang: str,
                  user: str,
-                 password: str):
-        self.host = host
-        self.service = service
-        self.group = group
-        self.sysname = sysname
-        self.client = client
-        self.lang = lang
-        self.user = user
-        self.password = password
+                 password: str,
+                 host: str = None,
+                 service: str = None,
+                 sysname: str = None,
+                 group: str = None):
+        self.params = {
+            'mshost': host,
+            'msserv': service,
+            'group': group,
+            'sysid': sysname,
+            'client': client,
+            'lang': lang,
+            'user': user,
+            'passwd': password,
+        }
 
     def get_connection(self) -> Connection:
-        return Connection(
-            user=self.user,
-            passwd=self.password,
-            mshost=self.host,
-            msserv=self.service,
-            group=self.group,
-            sysid=self.sysname,
-            client=self.client,
-            lang=self.lang
-        )
+        params = {k: v for k, v in self.params.items() if v is not None}
+        return Connection(**params)
